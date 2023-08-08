74c717495200f8f09033cd7c16ca312373dfd3a8
Yatharth <yatharth448@gmail.com>
2023-08-08 10:09:13 +0530

    name changed

--- ios/ShareWheelTests/ShareWheelTests.m
+++ ios/ShareWheelTests/ShareWheelTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface TravelAppTests : XCTestCase
+@interface ShareWheelTests : XCTestCase
 
 @end
 
-@implementation TravelAppTests
+@implementation ShareWheelTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
