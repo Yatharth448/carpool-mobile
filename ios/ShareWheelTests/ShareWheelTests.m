c8544c782301f44f38a8df65a34360c966576a80
Yatharth <yatharth448@gmail.com>
2023-08-08 10:06:48 +0530

    remane done

--- ios/ShareWheelTests/ShareWheelTests.m
+++ ios/ShareWheelTests/ShareWheelTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface carpoolTests : XCTestCase
+@interface ShareWheelTests : XCTestCase
 
 @end
 
-@implementation carpoolTests
+@implementation ShareWheelTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
