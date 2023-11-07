704eb8883d576a3ed413ab6f01dfce5e91cd4687
Sarthak Kaushik <skkaushik212@gmail.com>
2023-11-07 21:27:21 +0530

    Redirect changes

--- src/components/payment/PayGateway.js
+++ src/components/payment/PayGateway.js
@@ -1,47 +1,55 @@
-import React, { useState, useEffect } from 'react';
-import { View, Alert, Dimensions } from 'react-native';
-import { Picker } from '@react-native-picker/picker';
+import React, {useState, useEffect} from 'react';
+import {View, Alert, Dimensions} from 'react-native';
+import {Picker} from '@react-native-picker/picker';
 import axios from 'axios';
-import { WebView } from 'react-native-webview';
-import { Header } from '../commomheader/CommonHeader';
+import {WebView} from 'react-native-webview';
+import {Header} from '../commomheader/CommonHeader';
 
-const PayGateway = ({ navigation, route }) => {
+const PayGateway = ({navigation, route}) => {
+  const {payURL} = route.params;
+  const devWidth = Dimensions.get('screen').width;
+  const devHeight = Dimensions.get('screen').height;
+  const webView = React.useRef(null);
 
-    const { payURL } = route.params;
-    const devWidth = Dimensions.get('screen').width
-    const devHeight = Dimensions.get('screen').height
-    const webView = React.useRef(null);
+  useEffect(() => {
+    console.log(payURL, 'gateway');
+    // fetchData();
+  }, []);
 
-    useEffect(() => {
+  // webView ={
+  //     canGoBack: false,
+  //     injectedJavaScript: false,
+  //     ref: null
+  // }
 
-        console.log(payURL, 'gateway')
-        // fetchData();
-    }, []);
+  const onBack = () => {
+    console.log(webView, 'gateway');
+    navigation.goBack();
+  };
 
-    // webView ={
-    //     canGoBack: false,
-    //     injectedJavaScript: false,
-    //     ref: null
-    // }
-
-
-    const onBack = () => {
-        console.log(webView, 'gateway')
-        navigation.goBack()
-    }
-
-    return (
-        <>
-            <Header isBack={true} close={() => onBack()} />
-            <WebView
-                source={{ uri: payURL }}
-                ref={webView}
-                onLoadProgress={(event)=> console.log('event data', event.nativeEvent)}
-                onMessage={(event) => console.log("On Message", event)}
-                style={{ width: devWidth, height: devHeight }}
-            />
-        </>
-    );
+  return (
+    <>
+      <Header isBack={true} close={() => onBack()} />
+      <WebView
+        source={{uri: payURL}}
+        ref={webView}
+        onLoadProgress={event => {
+          console.log('Event ', event.nativeEvent.url);
+          let actualEvent = event.nativeEvent;
+          if (
+            actualEvent &&
+            actualEvent.title &&
+            actualEvent.title.indexOf('sharewheelz.in/api/auth/paysuccess') == 0
+          ) {
+            console.log('success event');
+            navigation.navigate('PaymentHistory');
+          }
+        }}
+        onMessage={event => console.log('On Message', event)}
+        style={{width: devWidth, height: devHeight}}
+      />
+    </>
+  );
 };
 
 export default PayGateway;
