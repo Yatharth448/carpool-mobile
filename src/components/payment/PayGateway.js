0f05e4168208fa22ca805c677aeaf1f987fd96d9
Sarthak Kaushik <skkaushik212@gmail.com>
2023-12-13 18:51:55 +0530

    Bug resolve

--- src/components/payment/PayGateway.js
+++ src/components/payment/PayGateway.js
@@ -2,7 +2,7 @@ import React, {useState, useEffect} from 'react';
 import {View, Alert, Dimensions, Platform} from 'react-native';
 import {Picker} from '@react-native-picker/picker';
 import axios from 'axios';
-import base64 from 'react-native-base64'
+import base64 from 'react-native-base64';
 import {WebView} from 'react-native-webview';
 import {Header} from '../commomheader/CommonHeader';
 
@@ -24,7 +24,12 @@ const PayGateway = ({navigation, route}) => {
   // }
 
   const onBack = () => {
-    console.log( base64.decode('SW4gbXkgZXllcywgaW5kaXNwb3NlZA0KSW4gZGlzZ3Vpc2VzIG5vIG9uZSBrbm93cw0KUklQIEND=='), 'decrypted data')
+    console.log(
+      base64.decode(
+        'SW4gbXkgZXllcywgaW5kaXNwb3NlZA0KSW4gZGlzZ3Vpc2VzIG5vIG9uZSBrbm93cw0KUklQIEND==',
+      ),
+      'decrypted data',
+    );
     console.log(webView, 'gateway');
     navigation.goBack();
   };
@@ -62,18 +67,25 @@ const PayGateway = ({navigation, route}) => {
           // console.log('Event ', event.nativeEvent.url);
           let actualEvent = event.nativeEvent;
           if (
-            actualEvent &&
-            Platform.OS == 'android' ? actualEvent.title : actualEvent.url &&
-            Platform.OS == 'android' ? actualEvent.title.indexOf('sharewheelz.in/api/auth/paysuccess') >= 0 : actualEvent.url.indexOf('sharewheelz.in/api/auth/paysuccess') >= 0
+            (actualEvent.title &&
+              Platform.OS == 'android' &&
+              actualEvent.title.indexOf('sharewheelz.in/api/auth/paysuccess') >=
+                0) ||
+            (actualEvent.url &&
+              Platform.OS == 'ios' &&
+              actualEvent.url.indexOf('sharewheelz.in/api/auth/paysuccess') >=
+                0)
           ) {
-           console.log()
-            console.log(actualEvent.url);
+            console.log('success event ', actualEvent.url);
             navigation.navigate('PaymentSuccess', {data: actualEvent.url});
-          }
-          else if (
-            actualEvent &&
-            Platform.OS == 'android' ? actualEvent.title : actualEvent.url &&
-            Platform.OS == 'android' ? actualEvent.title.indexOf('sharewheelz.in/api/auth/payfailed') >= 0 : actualEvent.url.indexOf('sharewheelz.in/api/auth/payfailed') >= 0
+          } else if (
+            (actualEvent.title &&
+              Platform.OS == 'android' &&
+              actualEvent.title.indexOf('sharewheelz.in/api/auth/payfailed') >=
+                0) ||
+            (actualEvent.url &&
+              Platform.OS == 'ios' &&
+              actualEvent.url.indexOf('sharewheelz.in/api/auth/payfailed') >= 0)
           ) {
             console.log('success event');
             navigation.navigate('PaymentFailure', {data: actualEvent.url});
